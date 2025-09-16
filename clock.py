f2e04a8afc1d2926786d9747b6f593802f5c1966
kristianche <chelebiev777@gmail.com>
2025-09-16 22:02:44 +0300

    Started creating a new console app: Clock

---
+++ clock.py
@@ -0,0 +1,17 @@
+import time
+
+print('### Welcome to Clock!                                     ###')
+
+time.sleep(1.5)
+
+print('### This is a console application which simulates a Timer ###')
+
+time.sleep(2.5)
+
+print('### Enjoy :)                                              ###')
+
+time.sleep(1)
+
+hours = int(input('Hours: '))
+minutes = int(input('Minutes: '))
+seconds = int(input('Seconds: '))
