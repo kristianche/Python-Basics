8f25e90e3f4c9bdcc4fda17e0537dc816d5481ff
kristianche <chelebiev777@gmail.com>
2025-09-17 17:27:27 +0300

    I have made the timer functionality

--- clock.py
+++ clock.py
@@ -12,6 +12,23 @@ print('### Enjoy :)                                              ###')
 
 time.sleep(1)
 
-hours = int(input('Hours: '))
-minutes = int(input('Minutes: '))
-seconds = int(input('Seconds: '))
+
+def timer():
+    hours = int(input('Hours: '))
+    minutes = int(input('Minutes: '))
+    seconds = int(input('Seconds: '))
+
+    total_seconds = (hours * 60) * 60 + minutes * 60 + seconds
+
+    for s in range(total_seconds, 0, -1):
+        hours_for_print = s // 3600
+        minutes_for_print = (s % 3600) // 60
+        seconds_for_print = s % 60
+
+        print(f"{hours_for_print:02d}:{minutes_for_print:02d}:{seconds_for_print:02d}")
+        time.sleep(1)
+
+    print("Time's up! ⏰")
+
+
+timer()
